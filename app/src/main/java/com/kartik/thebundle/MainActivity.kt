17d087b014be3cdc3cdf1d59b6a866488506618d
Kartik Kumar Gujarati <kartikkumar.gujarati@rackspace.com>
2018-03-27 20:04:06 -0500

    MainActivity with Toolbar widget

--- app/src/main/java/com/kartik/thebundle/MainActivity.kt
+++ app/src/main/java/com/kartik/thebundle/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.kartik.thebundle
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.widget.Toolbar
+import android.view.View
+import com.kartik.thebundle.onboarding.OnBoardingActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        //supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        val fab = findViewById(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener { view ->
+
+            startActivity(Intent(this, OnBoardingActivity::class.java))
+
+        }
     }
 }
